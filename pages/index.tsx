3cd30c0ddb593f53826ae438abc069654ae4767b
Ivan Morgillo <imorgillo@gmail.com>
2022-09-19 21:29:57 +0200

    feat: add alt text to avatar images
    Signed-off-by: Ivan Morgillo <imorgillo@gmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,20 +29,35 @@ const Home: NextPage = () => {
 
                 <div className={styles.grid}>
                     <a href="https://www.linkedin.com/in/ivanmorgillo/" className={styles.card}>
-                        <img src={"https://avatars.githubusercontent.com/hamen"} width={200} height={200} className={styles.avatar}/>
+                        <img src={"https://avatars.githubusercontent.com/hamen"}
+                             width={200}
+                             height={200}
+                             className={styles.avatar}
+                             alt={"Ivan Morgillo on a conference stage"}
+                        />
                         <h2>Ivan Morgillo 🍕🤌</h2>
                         <p>Consulente e trainer. Ho scritto un paio di libri, ho creato un po&apos; di app e parlo di Jetpack Compose il mercoledì
                             sera su <a href={"https://cwti.link/twitch"}>Twitch</a>.</p>
                     </a>
                     <a href="https://www.linkedin.com/in/matteo-principe/" className={styles.card}>
-                        <img src={"https://avatars.githubusercontent.com/readbeard"} width={200} height={200} className={styles.avatar}/>
+                        <img src={"https://avatars.githubusercontent.com/readbeard"}
+                             width={200}
+                             height={200}
+                             className={styles.avatar}
+                             alt={"Matteo Principe hiking"}
+                        />
                         <h2>Matteo Principe 🚀🚀</h2>
                         <p>Android Software Engineer <a href={"https://www.revolut.com/on-demand-pay/"}>@Revolut</a>. Amo il mondo start-up, la mia
                             barba e viaggiare zaino in spalla.
                             <br/>❤️ Mobile engineering su larga scala</p>
                     </a>
                     <a href="https://www.linkedin.com/in/marcocattaneo1/" className={styles.card}>
-                        <img src={"https://avatars.githubusercontent.com/u/764222"} width={200} height={200} className={styles.avatar}/>
+                        <img src={"https://avatars.githubusercontent.com/u/764222"}
+                             width={200}
+                             height={200}
+                             className={styles.avatar}
+                             alt={"Marco Cattaneo with palms in the background"}
+                        />
                         <h2>Marco Cattaneo 👾👾</h2>
                         <p>Android Software Engineer <a href={"https://www.moneyfarm.com/"}>@Moneyfarm</a>. Drogato di apprendimento:
                             se &egrave; qualcosa che non conosco devo saperne di pi&ugrave;!!<br/>Nel tempo libero amo cucinare 🍝 e fare lunghe
